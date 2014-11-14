2951d05d2f1bf7f503f27e268d40ffcd06eef176
krishds <krish.ds87@gmail.com>
2014-11-13 18:46:34 -0600

    send_main
    changed the addr

--- WSN/send_main.py
+++ WSN/send_main.py
@@ -11,7 +11,10 @@ from time import sleep
 if __name__ == "__main__":
     xbee = send_Xbee.send_Xbee(serialport = "COM10")
     sleep(1)
-    sent = xbee.Strng(data = 'Hello World', addr = 0x2191, options = 0x01, frameid = 0x00)
+    for x in range(0, 1000000):
+        sleep(2)
+        sent = xbee.Strng(data = 'Hello Krishna Lab 221', addr = 0x2191, options = 0x01, frameid = 0x00)
+        
     xbee.close()
 
     
