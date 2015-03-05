2bf57b647a2eaf670e80f01769a3ac4e994de9fb
krishds <krish.ds87@gmail.com>
2015-03-05 16:28:05 -0600

    code alignment
    code alignment

--- WSN/arduino/roomNode_Modified_Includes_Damp_Temp_Hum.ino
+++ WSN/arduino/roomNode_Modified_Includes_Damp_Temp_Hum.ino
@@ -29,31 +29,31 @@ Rx16Response rx = Rx16Response();
 
 short getTemperature1()
 {
-    int chk = DHT1.read22(DHT22_PIN_1);
-    short data = (short) (DHT1.temperature*100);
-    return data;
+	int chk = DHT1.read22(DHT22_PIN_1);
+	short data = (short) (DHT1.temperature*100);
+	return data;
 
 }
 
 short getTemperature2()
 {
-    int chk = DHT2.read22(DHT22_PIN_2);
-    short data = (short) (DHT2.temperature*100);
-    return data;	
+	int chk = DHT2.read22(DHT22_PIN_2);
+	short data = (short) (DHT2.temperature*100);
+	return data;	
 }
 
 short getHumidity1()
 {
-    int chk = DHT1.read22(DHT22_PIN_1);
-    short data = (short) (DHT1.humidity*100);
-    return data;
+	int chk = DHT1.read22(DHT22_PIN_1);
+	short data = (short) (DHT1.humidity*100);
+	return data;
 }
 
 short getHumidity2()
 {
-    int chk = DHT2.read22(DHT22_PIN_2);
-    short data = (short) (DHT2.humidity*100);
-    return data;
+	int chk = DHT2.read22(DHT22_PIN_2);
+	short data = (short) (DHT2.humidity*100);
+	return data;
 }
 
 int cntrlDamper(int data)
@@ -88,25 +88,25 @@ void loop()
 			xbee.getResponse().getRx16Response(rx);
 			data = rx.getData();
 			remoteID = rx.getRemoteAddress16();
-            dataLen = rx.getDataLength();
+			dataLen = rx.getDataLength();
 
-		    switch (data[0]) {
+			switch (data[0]) {
 
-		        case 116: // 116 --> ascii decimal equivalent of t
-	          	temp1 = getTemperature1();
-              	payload[0] = highByte(temp1);
+				case 116: // 116 --> ascii decimal equivalent of t
+				temp1 = getTemperature1();
+				payload[0] = highByte(temp1);
 				payload[1] = lowByte(temp1);
 
-              	temp2 = getTemperature2();
-              	payload[2] = highByte(temp2);
-              	payload[3] = lowByte(temp2);
+				temp2 = getTemperature2();
+				payload[2] = highByte(temp2);
+				payload[3] = lowByte(temp2);
 
 				tx.setAddress16(remoteID);
 				xbee.send(tx); 
-	            break;
+				break;
 
-		        case 104: // 104 --> ascii decimal equivalent of h
-	          	humid1 = getHumidity1();
+				case 104: // 104 --> ascii decimal equivalent of h
+				humid1 = getHumidity1();
 				payload[0] = highByte(humid1);
 				payload[1] = lowByte(humid1);
 
@@ -116,29 +116,30 @@ void loop()
 
 				tx.setAddress16(remoteID);
 				xbee.send(tx);
-	          	break;
+				break;
 
-	          	case 100: // 100 --> ascii decimal equivalent of d
-          		if(data[1] < 0 || data[1] > 100)
-      			{
-	   				Serial.print("Control Value OUT OF BOUNDS");
-	   				payload[4] = 0;
-      			}
-      			else
-      			{
-				   payload[4] = cntrlDamper(data[1]);
-				   tx.setAddress16(remoteID);
-				   xbee.send(tx);
-          		}
-	          	break;
+				case 100: // 100 --> ascii decimal equivalent of d
+				if(data[1] < 0 || data[1] > 100)
+				{
+					Serial.print("Control Value OUT OF BOUNDS");
+					payload[4] = 0;
+				}
+				else
+				{
+					payload[4] = cntrlDamper(data[1]);
+					tx.setAddress16(remoteID);
+					xbee.send(tx);
+				}
+				break;
 
-		        case 99: // 99 --> ascii decimal equivalent of h
-		          //int co2 = getCO2();
-	          	break;
+				case 99: // 99 --> ascii decimal equivalent of h
+				//int co2 = getCO2();
+				break;
 
-		        default:
-		        break;
-		    }
+				default:
+				payload[5] = {0,0,0,0,0};
+				break;
+			}
 		}
 
 	}
